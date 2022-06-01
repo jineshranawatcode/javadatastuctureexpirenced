d01322868c1fceda9cb5b2be5bf70e40ebfcb256
Bikin Maharjan <bikinmaharjan10@gmail.com>
2022-06-01 12:35:20 +1000

    feature

--- src/practisebikin/First.java
+++ src/practisebikin/First.java
@@ -4,5 +4,6 @@ public class First {
     public static void main(String[] args) {
         System.out.println("Hello First");
         System.out.println("Ok");
+        System.out.println("Feature");
     }
 }
