f4311c2ff36acf79d30011fda42ca8a741a6a5a0
Zac-Jewitt <zacjewitt22@gmail.com>
2022-06-01 12:35:29 +1000

    Changes

--- src/practise/First.java
+++ src/practise/First.java
@@ -2,6 +2,6 @@ package practise;
 
 public class First {
     public static void main(String args[]){
-        System.out.println("Hello everyone");
+        System.out.println("Hello everyone!");
     }
 }
